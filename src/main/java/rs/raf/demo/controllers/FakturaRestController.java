175ee9c16cd8a69f1061efb7de1e0c6d1f1df578
dvasiljevic9118rn <101745890+dvasiljevic9118rn@users.noreply.github.com>
2022-03-23 02:14:14 +0100

    Dodato filtriranje faktura

--- src/main/java/rs/raf/demo/controllers/FakturaRestController.java
+++ src/main/java/rs/raf/demo/controllers/FakturaRestController.java
@@ -1,13 +1,17 @@
 package rs.raf.demo.controllers;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import rs.raf.demo.model.Faktura;
 import rs.raf.demo.services.IFakturaService;
 import rs.raf.demo.services.impl.FakturaService;
+import rs.raf.demo.specifications.FakturaSpecificationsBuilder;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @CrossOrigin
 @RestController
@@ -47,6 +51,26 @@ public class FakturaRestController {
         }else{
             return ResponseEntity.ok(fakturaService.findAll());
         }
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> search(@RequestParam(name = "search") String search){
+        FakturaSpecificationsBuilder builder = new FakturaSpecificationsBuilder();
+        Pattern pattern = Pattern.compile("(\\w+?)(:|<|>)(\\w+?),");
+        Matcher matcher = pattern.matcher(search + ",");
+        while (matcher.find()) {
+            builder.with(matcher.group(1), matcher.group(2), matcher.group(3));
+        }
+
+        Specification<Faktura> spec = builder.build();
+        List<Faktura> result = fakturaService.findAll(spec);
+
+        if(result.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(result);
+
     }
 
 }
